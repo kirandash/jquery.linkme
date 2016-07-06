f35a4101d3cc1663c270aee4a9036aa3318aca79
kirandash <thekirankumardash@gmail.com>
2016-07-06 21:27:13 +0530

    Create the main plugin file

--- jquery.linkme.js
+++ jquery.linkme.js
@@ -1,1 +1,33 @@
-// JavaScript Document
+/*
+* name:		Linkme
+* author:	Kiran Dash
+* version:	1.0.0
+* license:	MIT
+*/
+
+(function($){
+	$.fn.linkme = function(options){
+		//Default Settings
+		var settings = $.extend({
+			href	:null,
+			text	:null,
+			target	:'_self'
+		}, options);
+		
+		//Validation
+		if(settings.href == null){
+			console.log('You need an href option for Linkme to work');
+			return this;
+		}
+		
+		return this.each(function(){
+			var object = $(this);
+			
+			if(settings.text == null){
+				settings.text = object.text();
+			}
+			
+			object.wrap('<a href="'+settings.href+'" target="'+settings.target+'"></a>').text(settings.text);
+		});
+	}
+}(jQuery));
